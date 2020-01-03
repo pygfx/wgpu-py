9b447d075739dd073658ec143dcfd2722f9b20f6
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2020-01-03 07:35:34 +0100

    revert to just the updated error message

--- wgpu/gui/qt.py
+++ wgpu/gui/qt.py
@@ -3,53 +3,33 @@ Support for rendering in a Qt widget. Provides a widget subclass that
 can be used as a standalone window or in a larger GUI.
 """
 
-from importlib import import_module
 import sys
 
 from .base import BaseCanvas
 
 
-# Select Qt toolkit
-libs = ("PySide2", "PyQt5")
-QWidget = None
-for libname in libs:
-    # if one of the libs is already imported, use that
+# Select GUI toolkit
+for libname in ("PySide2", "PyQt5"):
     if libname in sys.modules:
         QWidget = sys.modules[libname].QtWidgets.QWidget
         break
 else:
-    # otherwise, see which libs are available
-    available = {}
-    for libname in libs:
-        try:
-            available[libname] = import_module(libname + ".QtWidgets").QWidget
-        except ImportError:
-            pass
-    if len(available) == 1:
-        # if only one is available, use that
-        QWidget = list(available.values())[0]
-    else:
-        # otherwise error out and force the user to make a choice
-        raise ImportError(
-            "Import one of " + ", ".join(available.keys()) + " before "
-            "the WgpuCanvas to select a Qt toolkit"
-        )
-if QWidget is None:
-    raise ImportError("Could not import " + " or ".join(libs))
+    raise ImportError("Import one of PySide2, PyQt5 before the WgpuCanvas to select a Qt toolkit")
 
 
 class WgpuCanvas(BaseCanvas, QWidget):
     """ A QWidget subclass that can be used as a canvas to render to.
     """
 
-    def __init__(self, *args, size=(640, 480), title="", **kwargs):
+    def __init__(self, *args, size=None, title=None, **kwargs):
         super().__init__(*args, **kwargs)
         WA_PaintOnScreen = 8  # QtCore.Qt.WA_PaintOnScreen
         self.setAttribute(WA_PaintOnScreen, True)
         self.setAutoFillBackground(False)
 
-        width, height = size
-        self.resize(width, height)
+        if size:
+            width, height = size
+            self.resize(width, height)
         if title:
             self.setWindowTitle(title)
         self.show()
