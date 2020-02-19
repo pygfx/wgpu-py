604551c14935f2dbcfd643a53d036b4b528912a7
Korijn van Golen <korijn@gmail.com>
2020-02-19 23:49:07 +0100

    Update hook-wgpu.py

--- wgpu/__pyinstaller/hook-wgpu.py
+++ wgpu/__pyinstaller/hook-wgpu.py
@@ -2,3 +2,4 @@ from PyInstaller.utils.hooks import collect_data_files, collect_dynamic_libs
 
 datas = collect_data_files("wgpu")
 binaries = collect_dynamic_libs("wgpu")
+hiddenimports = ['wgpu.resources']
