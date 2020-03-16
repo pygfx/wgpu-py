3ad8f32ee1ead03848d19cdf3e540b4dc708da26
Almar Klein <almar.klein@gmail.com>
2020-03-16 22:02:04 +0100

    improve logging behavior

--- wgpu/gui/base.py
+++ wgpu/gui/base.py
@@ -1,7 +1,9 @@
 import os
 import sys
-import traceback
+import logging
 import ctypes.util
+
+logger = logging.getLogger("wgpu")
 
 
 class BaseCanvas:
@@ -34,24 +36,19 @@ class BaseCanvas:
         """ Draw the frame and present the swapchain. Errors are printed to stderr.
         Should be called by the subclass at an appropriate time.
         """
+        # Perform the user-defined drawing code. When this errors,
+        # we should report the error and then continue, otherwise we crash.
         try:
             self.draw_frame()
+        except Exception as err:
+            logger.error("Error during draw", exc_info=err)
+
+        # Always present the swapchain, or wgpu gets into an error state.
+        try:
             if self._swap_chain is not None:
                 self._swap_chain._gui_present()  # a.k.a swap buffers
-        except Exception:
-            # Enable PM debuging
-            sys.last_type, sys.last_value, sys.last_traceback = sys.exc_info()
-            msg = str(sys.last_value)
-            msgh = hash(msg)
-            if msgh in self._err_hashes:
-                count = self._err_hashes[msgh] + 1
-                self._err_hashes[msgh] = count
-                shortmsg = msg.split("\n", 1)[0].strip()[:50]
-                sys.stderr.write(f"Error in draw again ({count}): {shortmsg}\n")
-            else:
-                self._err_hashes[msgh] = 1
-                sys.stderr.write(f"Error in draw: " + msg.strip() + "\n")
-                traceback.print_last(6)
+        except Exception as err:
+            logger.error("Could not present the swapchain", exc_info=err)
 
     # Methods that must be overloaded
 
