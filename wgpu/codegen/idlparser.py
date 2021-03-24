d3873fabe81da98e9e34614a2ee92e1464145978
Almar Klein <almar@almarklein.org>
2021-03-24 15:41:22 +0100

    move codegen into the lib

---
+++ wgpu/codegen/idlparser.py
@@ -0,0 +1,343 @@
+"""
+The logic to parse the IDL file, from this, we generate the API.
+"""
+
+from .utils import to_python_name, to_neutral_name
+
+
+class StructField:
+    """A little object to specify the field of a struct."""
+
+    def __init__(self, line, name, typename, default=None):
+        self.line = line
+        self.name = name
+        self.typename = typename
+        self.default = default
+
+    def __repr__(self):
+        return f"<StructField '{self.typename} {self.name}'>"
+
+    def to_str(self):
+        return self.line
+
+    def py_arg(self):
+        name = to_python_name(self.name)
+        t = self.typename
+        d = self.default
+        if t not in ("bool", "int", "float", "str"):
+            t = f"'{t}'"
+        if d is not None:
+            d = {"false": "False", "true": "True"}.get(d, d)
+            return f"{name}: {t}={d}"
+        else:
+            return f"{name}: {t}"
+
+
+class Interface:
+    """A class definition, or flags."""
+
+    def __init__(self, name, bases):
+        self.bases = bases
+        self.constants = {}
+        self.attributes = {}  # name -> line
+        self.functions = {}
+
+
+class IdlParser:
+    """An object that can be used to walk over a str in an easy way.
+
+    This parser has the following attributes:
+
+    * flags: a dict mapping the (neutral) flag name to a dict of field-value pairs.
+    * enums: a dict mapping the (Pythonic) enum name to a dict of field-value pairs.
+    * structs: a dict mapping the (Pythonic) struct name to a dict of StructField
+      objects.
+    * functions: a dict mapping the (normalized) func name to the line defining the
+      function.
+
+    """
+
+    def __init__(self, text):
+        self._text = self._pre_process(text)
+        self._length = len(self._text)
+        self._pos = 0
+
+    def _reset(self):
+        self._pos = 0
+
+    def end_reached(self):
+        return self._pos >= self._length
+
+    def read_until(self, char):
+        start = self._pos
+        while self._pos < self._length:
+            c = self._text[self._pos]
+            self._pos += 1
+            if c == char:
+                return self._text[start : self._pos]
+        return ""
+
+    def read_line(self):
+        return self.read_until("\n")
+
+    def peek_line(self):
+        char = "\n"
+        start = pos = self._pos
+        while pos < self._length:
+            c = self._text[pos]
+            pos += 1
+            if c == char:
+                return self._text[start:pos]
+        return ""
+
+    def parse(self, verbose=False):
+
+        self._interfaces = {}
+        self.classes = {}
+        self.structs = {}
+        self.flags = {}
+        self.enums = {}
+
+        if verbose:
+            print(f"##### Parsing IDL ...")
+
+        self._reset()
+        self._parse()
+        self._post_process()
+
+        if verbose:
+            func_count = sum(len(cls.functions) for cls in self.classes.values())
+            print(f"Found {len(self.classes)} classes with {func_count} functions")
+            for thing in ["structs", "flags", "enums"]:
+                print(f"Found {len(getattr(self, thing))} {thing}")
+
+    def _pre_process(self, text):
+        """Pre-process the text to make it a bit easier to parse.
+        Beware to keep line numbers the same
+        """
+        text = text.replace("\n[\n", "\n\n[").replace("\n]\n", "]\n\n")
+        text = text.replace("[    ", "[")
+        text = self._remove_comments(text)
+        return text
+
+    def _remove_comments(self, text):
+        lines = []
+        in_multiline_comment = False
+        for line in text.splitlines():
+            if in_multiline_comment:
+                if "*/" in line:
+                    _, _, line = line.partition("//")
+                    if "//" in line:
+                        line, _, _ = line.partition("//")
+                    lines.append(line if line.strip() else "")
+                    in_multiline_comment = False
+                else:
+                    lines.append("")
+            else:
+                if "//" in line:
+                    line, _, _ = line.partition("//")
+                    lines.append(line if line.strip() else "")
+                elif "/*" in line:
+                    line, _, _ = line.partition("/*")
+                    lines.append(line if line.strip() else "")
+                    in_multiline_comment = True
+                else:
+                    lines.append(line)
+        return "\n".join(lines)
+
+    def _parse(self):
+
+        typedefs = {}
+
+        while not self.end_reached():
+
+            line = self.read_line()
+
+            if not line.strip():
+                pass
+            elif line.startswith("typedef "):
+                pass
+            elif line.startswith(("interface ", "partial interface ")):
+                # A class or a set of flags
+                # Collect lines that define this interface
+                lines = [line]
+                while not line.startswith("};"):
+                    line = self.read_line()
+                    lines.append(line)
+                classname = lines[0].split("{")[0].split(":")[0].split()[-1]
+                # Collect base classes
+                based_on = []
+                while self.peek_line().startswith(classname + " includes "):
+                    line = self.read_line()
+                    based_on.append(line.split()[-1].rstrip(";"))
+                # Create / get interface object
+                if classname not in self._interfaces:
+                    self._interfaces[classname] = Interface(classname, based_on)
+                interface = self._interfaces[classname]
+                # Parse members
+                line_index = 0
+                while line_index < len(lines) - 1:
+                    line_index += 1
+                    line = lines[line_index].strip()
+                    if not line:
+                        continue
+                    elif line.startswith("[Exposed="):
+                        continue  # WTF?
+                    elif line.startswith("const "):
+                        parts = line.strip(";").split()
+                        assert len(parts) == 5
+                        assert parts[-2] == "="
+                        name = parts[2]
+                        val = int(parts[-1], 16)
+                        interface.constants[name] = val
+                    elif "attribute " in line:
+                        name = line.partition("attribute")[2].split()[-1].strip(";")
+                        interface.attributes[name] = line
+                    elif "(" in line:
+                        line = lines[line_index]
+                        while line.count("(") > line.count(")"):
+                            line_index += 1
+                            line += lines[line_index]
+                        assert line.count("(") == line.count(")")
+                        line = line.strip()
+                        line.replace("\n", " ")
+                        for c in ("    ", "  ", "  "):
+                            line = line.replace(c, " ")
+                        assert line.endswith(";")
+                        funcname = line.split("(")[0].split()[-1]
+                        line = (
+                            line.replace("\n", " ")
+                            .replace("    ", " ")
+                            .replace("  ", " ")
+                        )
+                        interface.functions[funcname] = line
+            elif line.startswith("enum "):
+                line += self.read_until("}") + self.read_line()
+                lines = line.strip().split("\n")
+                name = lines[0].split(" ", 1)[1].strip("{ \t\r\n")
+                d = {}
+                for i, line in enumerate(lines[1:-1]):
+                    line = line.strip()
+                    if not line or line.startswith("//"):
+                        continue
+                    key = val = line.strip('", \t')
+                    for i1, i2 in [
+                        ("-", "_"),
+                        ("1d", "d1"),
+                        ("2d", "d2"),
+                        ("3d", "d3"),
+                    ]:
+                        key = key.replace(i1, i2)
+                    d[key] = val
+                self.enums[name] = d
+            elif line.startswith("dictionary "):
+                assert line.count("{") == 1 and line.count("}") == 0
+                lines = [line]
+                while not line.startswith("};"):
+                    line = self.read_line()
+                    lines.append(line)
+                name = lines[0].split(" ", 1)[1].strip("{ \t\r\n")
+                if ":" in name:
+                    name, _, base = name.partition(":")
+                    name, base = name.strip(), base.strip()
+                    if base not in self.structs:
+                        # print(f"dict {name} has unknown base dict {base}")
+                        d = {}
+                    else:
+                        d = self.structs[base].copy()
+                else:
+                    d = {}
+                for line in lines[1:-1]:
+                    line = line.split("//")[0].strip()
+                    if not line:
+                        continue
+                    assert line.endswith(";")
+                    arg = line.strip().strip(",;").strip()
+                    default = None
+                    if "=" in arg:
+                        arg, default = arg.rsplit("=", 1)
+                        arg, default = arg.strip(), default.strip()
+                    arg_type, arg_name = arg.strip().rsplit(" ", 1)
+                    if arg_type.startswith("required "):
+                        arg_type = arg_type[9:]
+                        # required args should not have a default
+                        assert default is None
+                    else:
+                        default = default or "None"
+                    arg_type = typedefs.get(arg_type, arg_type)
+                    if arg_type in ["double", "float"]:
+                        t = "float"
+                    elif arg_type in [
+                        "long",
+                        "unsigned long",
+                        "unsigned long long",
+                        "GPUSize64",
+                        "[Clamp] unsigned short",
+                    ]:
+                        t = "int"
+                    elif arg_type in ["boolean"]:
+                        t = "bool"
+                    elif arg_type in ["DOMString", "DOMString?", "USVString"]:
+                        t = "str"
+                    elif arg_type in ["object", "record<DOMString, GPUSize32>"]:
+                        t = "dict"
+                    elif arg_type.startswith("GPU"):
+                        t = arg_type
+                    elif arg_type.startswith("sequence<GPU"):
+                        t = arg_type[9:-1] + "-list"
+                    elif arg_type == "ImageBitmap":
+                        t = "array"
+                    elif arg_type in [
+                        "(GPULoadOp or GPUColor)",
+                        "(GPULoadOp or GPUStencilValue)",
+                        "(GPULoadOp or float)",
+                        "(GPULoadOp or unsigned long)",
+                    ]:
+                        # GPURenderPassColorAttachmentDescriptor
+                        # GPURenderPassDepthStencilAttachmentDescriptor
+                        t = (
+                            arg_type[1:-1]
+                            .replace(" ", "-")
+                            .replace("unsigned-long", "int")
+                        )
+                    else:
+                        assert False
+                    d[arg_name] = StructField(line, arg_name, t, default)
+                self.structs[name] = d
+            elif line.startswith(("[Exposed=", "[Serializable]")):
+                pass
+            else:
+                raise RuntimeError("Unknown line:", line.rstrip())
+
+    def _post_process(self):
+        """We don't do any name format normalization in the parser code itself;
+        we do that here.
+        """
+
+        # Divide flags and actual class definitions
+        for name, interface in self._interfaces.items():
+            if interface.constants:
+                self.flags[name] = interface.constants
+            elif name not in ("Navigator", "WorkerNavigator"):
+                delattr(interface, "constants")
+                self.classes[name] = interface
+
+        # Remove GPU prefix
+        for d in (self.structs, self.flags, self.enums):
+            for name in list(d.keys()):
+                assert name.startswith("GPU")
+                new_name = name[3:]
+                if new_name.endswith("Dict"):
+                    new_name = new_name[:-4]
+                d[new_name] = d.pop(name)
+
+        # Remove (abstract) base structs
+        for name in list(self.structs):
+            if name.endswith("Base"):
+                self.structs.pop(name)
+
+        # Turn funcs into python name
+        for cls in self.classes.values():
+            for funcname in list(cls.functions.keys()):
+                funcname2 = to_python_name(funcname)
+                cls.functions[funcname2] = cls.functions.pop(funcname)
---
+++ wgpu/codegen/utils.py
@@ -0,0 +1,213 @@
+import sys
+
+import black
+
+
+def to_neutral_name(name):
+    """Convert a name to the neutral name with no capitals, underscores or dots.
+    Used primarily to match function names of IDL and .h specs.
+    """
+    name = name.lower()
+    if name.startswith("gpu"):
+        name = name[3:]
+    if name.startswith("wgpu"):
+        name = name[4:]
+    for c in " -_.":
+        name = name.replace(c, "")
+    return name
+
+
+def to_python_name(name):
+    """Convert someName and some_name to the Python flavor.
+    To convert function names and function argument names.
+    """
+    name2 = ""
+    for c in name:
+        c2 = c.lower()
+        if c2 != c and len(name2) > 0 and name2[-1] != "_":
+            name2 += "_"
+        name2 += c2
+    return name2
+
+
+def blacken(src, singleline=False):
+    """Format the given src string using black. If singleline is True,
+    all function signatures become single-line, so they can be parsed
+    and updated.
+    """
+    # Normal black
+    result = black.format_str(src, mode=black.FileMode())
+
+    # Make defs single-line. You'd think that setting the line length
+    # to a very high number would do the trick, but it does not.
+    if singleline:
+        lines1 = result.splitlines()
+        lines2 = []
+        in_sig = False
+        comment = ""
+        for line in lines1:
+            if in_sig:
+                # Handle comment
+                line, _, c = line.partition("#")
+                line = line.rstrip()
+                c = c.strip()
+                if c:
+                    comment += " " + c.strip()
+                # Detect end
+                if line.endswith("):"):
+                    in_sig = False
+                # Compose line
+                current_line = lines2[-1]
+                if not current_line.endswith("("):
+                    current_line += " "
+                current_line += line.lstrip()
+                # Finalize
+                if not in_sig:
+                    # Remove trailing spaces and commas
+                    current_line = current_line.replace(" ):", "):")
+                    current_line = current_line.replace(",):", "):")
+                    # Add comment
+                    if comment:
+                        current_line += "  #" + comment
+                        comment = ""
+                lines2[-1] = current_line
+            else:
+                lines2.append(line)
+                line_nc = line.split("#")[0].strip()
+                if line_nc.startswith(("def ", "class ")) and "(" in line_nc:
+                    if not line_nc.endswith("):"):
+                        in_sig = True
+        lines2.append("")
+        result = "\n".join(lines2)
+
+    return result
+
+
+class Patcher:
+    """Class to help patch a Python module. Supports iterating (over
+    lines, classes, functions), and applying diffs (replace, remove, insert).
+    """
+
+    def __init__(self, text_or_filename):
+        if "\n" in text_or_filename:
+            self._filename = None
+            text = text_or_filename
+        else:
+            self._filename = text_or_filename
+            with open(text_or_filename, "rb") as f:
+                text = f.read().decode()
+        self.lines = blacken(text, True).splitlines()  # inf line length
+        self._diffs = {}
+        self._classes = {}
+
+    def remove_line(self, i):
+        assert i not in self._diffs, f"Line {i} already has a diff"
+        self._diffs[i] = i, "remove"
+
+    def insert_line(self, i, line):
+        if i in self._diffs and self._diffs[i][1] == "insert":
+            cur_line = self._diffs[i][2]
+            self._diffs[i] = i, "insert", cur_line + "\n" + line
+        else:
+            assert i not in self._diffs, f"Line {i} already has a diff"
+            self._diffs[i] = i, "insert", line
+
+    def replace_line(self, i, line):
+        assert i not in self._diffs, f"Line {i} already has a diff"
+        self._diffs[i] = i, "replace", line
+
+    def dumps(self, format=True):
+        lines = self.lines.copy()
+        # Apply diff
+        diffs = sorted(self._diffs.values())
+        for diff in reversed(diffs):
+            if diff[1] == "remove":
+                lines.pop(diff[0])
+            elif diff[1] == "insert":
+                lines.insert(diff[0], diff[2])
+            elif diff[1] == "replace":
+                lines[diff[0]] = diff[2]
+            else:
+                raise ValueError(f"Unknown diff: {diff}")
+        # Format
+        text = "\n".join(lines)
+        if format:
+            text = blacken(text)
+        return text
+
+    def dump(self, format=True):
+        assert self._filename, "Was not given a filename during init"
+        text = self.dumps(format)
+        with open(self._filename, "wb") as f:
+            f.write(text.encode())
+
+    def iter_lines(self, start_line=0):
+        for i in range(start_line, len(self.lines)):
+            line = self.lines[i]
+            yield line, i
+
+    def iter_classes(self, start_line=0):
+        current_class = None
+        for i in range(start_line, len(self.lines)):
+            line = self.lines[i]
+            sline = line.rstrip()
+            if sline and not sline.startswith("    "):
+                if current_class:
+                    yield current_class + (i - 1,)
+                    current_class = None
+            if line.startswith("class "):
+                name = line.split(":")[0].split("(")[0].split()[-1]
+                current_class = name, i
+                # self._classes[current_class] = i, current_class, {}
+        if current_class:
+            yield current_class + (i - 1,)
+
+    def iter_properties(self, start_line=0):
+        return self._iter_props_and_methods(start_line, True)
+
+    def iter_methods(self, start_line=0):
+        return self._iter_props_and_methods(start_line, False)
+
+    def _iter_props_and_methods(self, start_line, find_props):
+        prop_mark = None
+        current_def = None
+        for i in range(start_line, len(self.lines)):
+            line = self.lines[i]
+            sline = line.rstrip()
+            if sline and not sline.startswith("        "):
+                if current_def:
+                    yield current_def + (i - 1,)
+                    current_def = None
+                if not sline.startswith("    "):
+                    break  # exit class
+            if line.startswith("    def "):
+                name = line.split("(")[0].split()[-1]
+                if prop_mark and find_props:
+                    current_def = name, prop_mark
+                elif not prop_mark and not find_props:
+                    current_def = name, i
+            if line.startswith("    @property"):
+                prop_mark = i
+            elif sline and not sline.startswith("#"):
+                prop_mark = None
+
+        if current_def:
+            yield current_def + (i - 1,)
+
+    def parse_public_api(self):
+        current_class = None
+        for i, line in enumerate(self.lines):
+            if line.startswith("class "):
+                current_class = line.split(":")[0].split("(")[0].split()[-1]
+                self._classes[current_class] = i, current_class, {}
+            if line.lstrip().startswith(("def ", "async def")):
+                indent = len(line) - len(line.lstrip())
+                funcname = line.split("(")[0].split()[-1]
+                if not funcname.startswith("_"):
+                    if not self.lines[i - 1].lstrip().startswith("@property"):
+                        func_id = funcname
+                        funcname = to_python_name(funcname)
+                        if indent:
+                            func_id = current_class + "." + func_id
+                        func_id = to_neutral_name(func_id)
+                        api_functions[func_id] = funcname, i, indent
