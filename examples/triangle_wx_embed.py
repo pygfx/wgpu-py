8d4556d970a34ec573fdaa4942ed8537fa8fe0c6
Almar Klein <almar@almarklein.org>
2021-03-31 09:54:20 +0200

    Add examples to embed in qt and wx

---
+++ examples/triangle_wx_embed.py
@@ -0,0 +1,40 @@
+"""
+An example demonstrating a wx app with a wgpu viz inside.
+"""
+
+import wx
+from wgpu.gui.wx import WgpuCanvas
+import wgpu.backends.rs  # noqa: F401, Select Rust backend
+
+from examples.triangle import main
+
+
+class Example(wx.Frame):
+    def __init__(self):
+        super().__init__(None, title="wgpu triangle embedded in a wx app")
+        self.SetSize(640, 480)
+
+        splitter = wx.SplitterWindow(self)
+
+        self.button = wx.Button(self, -1, "Hello world")
+        self.canvas1 = WgpuCanvas(splitter)
+        self.canvas2 = WgpuCanvas(splitter)
+
+        splitter.SplitVertically(self.canvas1, self.canvas2)
+        splitter.SetSashGravity(0.5)
+
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer.Add(self.button, 0, wx.EXPAND)
+        sizer.Add(splitter, 1, wx.EXPAND)
+        self.SetSizer(sizer)
+
+        self.Show()
+
+
+app = wx.App()
+example = Example()
+
+main(example.canvas1)
+main(example.canvas2)
+
+app.MainLoop()
