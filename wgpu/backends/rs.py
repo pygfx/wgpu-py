a5a21b24905d62b0852af9c64d5e17ca0ff386d8
Almar Klein <almar@almarklein.org>
2021-06-04 21:55:37 +0200

    update to latest wgpu-native

--- wgpu/backends/rs.py
+++ wgpu/backends/rs.py
@@ -53,8 +53,8 @@ logger = logging.getLogger("wgpu")  # noqa
 apidiff = ApiDiff()
 
 # The wgpu-native version that we target/expect
-__version__ = "0.8.0.1"
-__commit_sha__ = "691468c9817b51a81530fb277a9b30fcc4a71ea7"
+__version__ = "0.8.0.2"
+__commit_sha__ = "66a4139579f2499a67b7ade1a6c3bcb414046c64"
 version_info = tuple(map(int, __version__.split(".")))
 check_expected_version(version_info)  # produces a warning on mismatch
 
