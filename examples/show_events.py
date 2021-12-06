eb4896f87638d7535ecee999c15d14cf4dd9dd51
Almar Klein <almar@almarklein.org>
2021-12-06 21:28:41 +0100

    add example

---
+++ examples/show_events.py
@@ -0,0 +1,16 @@
+"""
+A simple example to demonstrate events.
+"""
+
+from wgpu.gui.auto import WgpuCanvas, run
+
+
+class MyCanvas(WgpuCanvas):
+    def handle_event(self, event):
+        if event["event_type"] != "pointer_move":
+            print(event)
+
+
+if __name__ == "__main__":
+    canvas = MyCanvas(size=(640, 480), title="wgpu triangle with GLFW")
+    run()
