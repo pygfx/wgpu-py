93af9c54d90643488b1edacfb692d38e5c85855a
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-01-14 16:46:56 +0100

    Add Qt as auto gui

--- examples/events.py
+++ examples/events.py
@@ -1,8 +1,14 @@
 """
 A simple example to demonstrate events.
 """
-
-from wgpu.gui.auto import WgpuCanvas, run
+try:
+    # Try importing PySide6 to use PySide6 for
+    # auto gui. If PySide6 can't be imported
+    # then glfw is used as fallback
+    import PySide6  # noqa
+except ImportError:
+    pass
+from wgpu.gui.auto import WgpuCanvas, run, call_later
 
 
 class MyCanvas(WgpuCanvas):
@@ -13,4 +19,10 @@ class MyCanvas(WgpuCanvas):
 
 if __name__ == "__main__":
     canvas = MyCanvas(size=(640, 480), title="wgpu events")
+
+    def send_message(message):
+        print(f"Message: {message}")
+
+    call_later(2, send_message, "hello")
+
     run()
--- wgpu/gui/auto.py
+++ wgpu/gui/auto.py
@@ -25,11 +25,28 @@ def is_jupyter():
 if is_jupyter():
     from .jupyter import WgpuCanvas, run, call_later  # noqa
 else:
-    try:
-        from .glfw import WgpuCanvas, run, call_later  # noqa
-    except ImportError as err:
-        msg = str(err)
-        msg += "\n\n  Install glfw using e.g. ``pip install -U glfw``."
-        if sys.platform.startswith("linux"):
-            msg += "\n  You may also need to run the equivalent of ``apt install libglfw3``."
-        raise ImportError(msg) from None
+    for libname in ("PySide6", "PyQt6", "PySide2", "PyQt5", "PySide", "PyQt4"):
+        if libname in sys.modules:
+            from .qt import WgpuCanvas, QtWidgets, QtCore  # noqa
+
+            # When using Qt, there needs to be an
+            # application before any widget is created
+            app = QtWidgets.QApplication([])
+
+            def run():
+                app.exec() if hasattr(app, "exec") else app.exec_()
+
+            def call_later(delay, callback, *args):
+                QtCore.QTimer.singleShot(delay * 1000, lambda: callback(*args))
+
+            break
+
+    else:
+        try:
+            from .glfw import WgpuCanvas, run, call_later  # noqa
+        except ImportError as err:
+            msg = str(err)
+            msg += "\n\n  Install glfw using e.g. ``pip install -U glfw``."
+            if sys.platform.startswith("linux"):
+                msg += "\n  You may also need to run the equivalent of ``apt install libglfw3``."
+            raise ImportError(msg) from None
