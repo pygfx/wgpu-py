52d63b1c941f6314f87e883b7e417c7be0d10feb
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2022-02-04 15:21:43 +0100

    Remove screenshot testing for now

--- conftest.py
+++
@@ -1,7 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption(
-        "--regenerate-screenshots",
-        action="store_true",
-        dest="regenerate_screenshots",
-        default=False,
-    )
