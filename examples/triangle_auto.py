05e00407c454560efb23bca3f7b5f73f687e4960
Almar Klein <almar@almarklein.org>
2022-04-12 16:53:37 +0200

    More example-tests (#258)
    * Add assertions to compute example
    
    * add testable triangle example
    
    * make importable
    
    * fiz
    
    * sigh - this is why I'm not a fan of pathlib...
    
    * add ci-generated screenshot
    
    * I wonder if this changes the order ...

---
+++ examples/triangle_auto.py
@@ -0,0 +1,22 @@
+"""
+Import the viz from triangle.py and run it using the auto-gui.
+"""
+# test_example = true
+
+import sys
+from pathlib import Path
+
+from wgpu.gui.auto import WgpuCanvas, run
+import wgpu.backends.rs  # noqa: F401, Select Rust backend
+
+sys.path.insert(0, str(Path(__file__).parent))
+
+from triangle import main  # noqa: E402, The function to call to run the visualization
+
+
+canvas = WgpuCanvas()
+device = main(canvas)
+
+
+if __name__ == "__main__":
+    run()
