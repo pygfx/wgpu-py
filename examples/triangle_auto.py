128bcd183e80ef821256f9113ae7b490a90e60b0
Almar Klein <almar@almarklein.org>
2022-04-12 15:50:28 +0200

    make importable

--- examples/triangle_auto.py
+++ examples/triangle_auto.py
@@ -3,9 +3,13 @@ Import the viz from triangle.py and run it using the auto-gui.
 """
 # test_example = true
 
+import sys
+from pathlib import Path
 
 from wgpu.gui.auto import WgpuCanvas, run
 import wgpu.backends.rs  # noqa: F401, Select Rust backend
+
+sys.path.insert(0, Path(__file__).parent.parent)
 
 from triangle import main  # The function to call to run the visualization
 
