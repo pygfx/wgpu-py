45ecae8f509d47051ddfa6690899df925a2ede62
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2022-04-15 10:30:40 +0200

    delay of 0

--- wgpu/gui/offscreen.py
+++ wgpu/gui/offscreen.py
@@ -37,7 +37,7 @@ class WgpuManualOffscreenCanvas(WgpuAutoGui, WgpuOffscreenCanvas):
         return False
 
     def _request_draw(self):
-        call_later(self.draw)
+        call_later(0, self.draw)
 
     def present(self, texture_view):
         # This gets called at the end of a draw pass via GPUCanvasContextOffline
