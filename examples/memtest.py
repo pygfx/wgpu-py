9898842bbfd3f5b360cfc187ca4806d56ede7a72
Korijn van Golen <korijn@gmail.com>
2022-11-11 09:28:38 +0100

    clean

--- examples/memtest.py
+++ examples/memtest.py
@@ -1,20 +1,23 @@
+# flake8: noqa
 import gc
 import os
 
 os.environ["WGPU_FORCE_OFFSCREEN"] = "true"
 
 import psutil
-import wgpu.backends.rs  # noqa: F401, Select Rust backend
+import wgpu.backends.rs
 from wgpu.gui.auto import WgpuCanvas, run
 
-from triangle import main  # The function to call to run the visualization
+from triangle import main
 
 
 p = psutil.Process()
 
+
 def print_mem_usage(i):
-    megs = p.memory_info().rss / 1024 ** 2
+    megs = p.memory_info().rss / 1024**2
     print(f"memory usage (round: {i}): {megs:.3f} MB")
+
 
 if __name__ == "__main__":
     print_mem_usage(0)
