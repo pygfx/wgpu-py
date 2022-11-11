1b5ba8e2d4f5f0510f7b57b2061f1f8884a7e2e7
Korijn van Golen <korijn@gmail.com>
2022-11-11 09:18:14 +0100

    check if leak is in wgpu-py

---
+++ examples/memtest.py
@@ -0,0 +1,26 @@
+import gc
+import os
+
+os.environ["WGPU_FORCE_OFFSCREEN"] = "true"
+
+import psutil
+import wgpu.backends.rs  # noqa: F401, Select Rust backend
+from wgpu.gui.auto import WgpuCanvas, run
+
+from triangle import main  # The function to call to run the visualization
+
+
+p = psutil.Process()
+
+def print_mem_usage(i):
+    megs = p.memory_info().rss / 1024 ** 2
+    print(f"memory usage (round: {i}): {megs:.3f} MB")
+
+if __name__ == "__main__":
+    print_mem_usage(0)
+    for i in range(10):
+        canvas = WgpuCanvas(size=(640, 480), title="wgpu triangle")
+        device = main(canvas)
+        run()
+        gc.collect()
+        print_mem_usage(i + 1)
