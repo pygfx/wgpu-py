256a99090ab8dab4cdd5e1e4fa28e8802b47729b
panxinmiao <panxinmiao@minivision.cn>
2023-02-17 10:07:56 +0800

    support GLSL in shadertoy

--- examples/shadertoy_circuits.py
+++ examples/shadertoy_circuits.py
@@ -42,7 +42,7 @@ fn fractal(p: vec2<f32>) -> vec3<f32> {
 }
 
 fn shader_main(frag_coord: vec2<f32>) -> vec4<f32> {
-    var uv = frag_coord / i_resolution - 0.5;
+    var uv = frag_coord / i_resolution.xy - 0.5;
     uv.x*=i_resolution.x/i_resolution.y;
 
     var aa = 6.0;
