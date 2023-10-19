b51092bdbe3ec98d1d56368baf14fc66e3b8580d
Almar Klein <almar@almarklein.org>
2023-10-19 11:12:28 +0200

    Update notes on Vulkan/D3D12

--- wgpu/backends/rs.py
+++ wgpu/backends/rs.py
@@ -219,12 +219,19 @@ class GPU(base.GPU):
         # the NVidia control panel settings.
         # See https://github.com/gfx-rs/wgpu/issues/1416
         # todo: for the moment we default to forcing Vulkan on Windows
-        # -> AK (19-10-2023): Tried using D3D12. An advantage is that it works
-        #    on a clean Windows install where Nvidia drivers have not been
-        #    upgraded yet. It runs some/most examples, but fails the 3D
-        #    tests in test_rs_compute_tex.py, produces an error for the
-        #    Naga-generated DX shader code on the volume rendering example,
-        #    and buffer mapping seems iffy too.
+        # -> AK (19-10-2023): Tried using D3D12.
+        #    With a clean Windows install (no Nvidia drivers installed yet):
+        #       Vulkan crashes when it tries to display a window.
+        #       Did not test D3D12.
+        #    With Nvidia drivers uninstalled:
+        #       Vulkan reports "no available adapter".
+        #       D3d12 kinda works. It runs some/most examples, but fails the 3D
+        #       tests in test_rs_compute_tex.py, produces an error for the
+        #       Naga-generated DX shader code on the volume rendering example,
+        #       and buffer mapping (e.g. in pygfx picking) seems iffy too.
+        #    With NVidia drivers:
+        #       Both Vulkan and D3D12 work as expected, passing all tests
+        #       (unit and examples) in wgpu-py and pygfx.
         force_backend = os.getenv("WGPU_BACKEND_TYPE", None)
         backend = enum_str2int["BackendType"]["Undefined"]
         if force_backend is None:  # Allow OUR defaults
