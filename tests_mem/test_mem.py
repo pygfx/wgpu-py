cb2cd2d61bef2ca01ff85195026310a70ddb9fe3
Almar Klein <almar@almarklein.org>
2023-11-02 17:02:37 +0100

    Progress on the memtests

--- tests_mem/test_mem.py
+++ tests_mem/test_mem.py
@@ -1,17 +1,29 @@
 import gc
 
 import wgpu.backends.rs
+import wgpu.gui.offscreen
+
 import pytest
+from testutils import can_use_glfw, can_use_wgpu_lib
+
+
+if not can_use_wgpu_lib:
+    pytest.skip(
+        "Skipping tests that need a window or the wgpu lib", allow_module_level=True
+    )
 
 
 # Create the default device beforehand
-wgpu.utils.get_default_device()
+DEVICE = wgpu.utils.get_default_device()
 
 
 # %% The logic
 
 
 def get_counts():
+    """Get a dict that maps object names to a 2-tuple represening
+    the counts in py and wgpu-native.
+    """
     counts_py = wgpu.diagnostics.object_counts.get_dict()
     counts_native = wgpu.diagnostics.rs_counts.get_dict()
 
@@ -30,94 +42,129 @@ def get_counts():
     return counts
 
 
-def compare_counts(counts1, counts2):
-    more_py = {}
-    more_native = {}
+def get_excess_counts(counts1, counts2):
+    """Compare two counts dicts, and return a new dict with the fields
+    that have increased counts.
+    """
+    more = {}
     for name in counts1:
         c1 = counts1[name][0]
         c2 = counts2[name][0]
+        more_py = 0
         if c2 > c1:
-            more_py[name] = c2 - c1
+            more_py = c2 - c1
         c1 = counts1[name][1]
         c2 = counts2[name][1]
+        more_native = 0
         if c2 > c1:
-            more_native[name] = c2 - c1
-    return more_py, more_native
+            more_native = c2 - c1
+        if more_py or more_native:
+            more[name] = more_py, more_native
+    return more
 
 
-def obname_from_test_func_name(func):
-    obname = func.__name__
+def ob_name_from_test_func(func):
+    """Translate test_release_bind_group() to "BindGroup"."""
+    func_name = func.__name__
     prefix = "test_release_"
-    assert obname.startswith(prefix)
-    return obname[len(prefix) :].replace("_", "").lower()
+    assert func_name.startswith(prefix)
+    words = func_name[len(prefix) :].split("_")
+    if words[-1].isnumeric():
+        words.pop(-1)
+    return "".join(word.capitalize() for word in words)
 
 
 def create_and_release(create_objects_func):
-    def _create_and_release():
+    """Decorator."""
+
+    def core_test_func():
+        """The core function that does the testing."""
+
         n = 32
 
-        # Before ...
+        generator = create_objects_func(n)
+
+        # ----- Collect options
+
+        options = {
+            "expected_native_high_count": n,
+            "allowed_native_low_count": 0,
+        }
+
+        func_options = next(generator)
+        assert isinstance(func_options, dict), "First yield must be an options dict"
+        options.update(func_options)
+
+        # Measure baseline object counts
         counts1 = get_counts()
 
+        # ----- Create
+
         # Create objects
-        objects = list(create_objects_func(n))
+        objects = list(generator)
 
-        # Test the count and that all are of the same class
+        # Test the count
         assert len(objects) == n
-        assert all(
-            isinstance(objects[i], objects[0].__class__) for i in range(len(objects))
-        )
-        class_name = objects[0].__class__.__name__[3:].lower()
 
-        # Test that cass matches function name (should prevent a group of copy-paste errors)
-        obname = obname_from_test_func_name(create_objects_func)
-        assert obname == class_name
+        # Test that all objects are of the same class.
+        # (this for-loop is a bit weird, but its to avoid leaking refs to objects)
+        cls = objects[0].__class__
+        assert all(isinstance(objects[i], cls) for i in range(len(objects)))
 
+        # Test that class matches function name (should prevent a group of copy-paste errors)
+        ob_name = ob_name_from_test_func(create_objects_func)
+        assert ob_name == cls.__name__[3:]
+
+        # Measure peak object counts
         counts2 = get_counts()
-        more_py, more_native = compare_counts(counts1, counts2)
+        more2 = get_excess_counts(counts1, counts2)
+        print("  more after create:", more2)
 
-        print("  more_py", more_py)
-        print("  more_native", more_native)
+        # Make sure the actual object has increased
+        expected_high_count = n, options["expected_native_high_count"]
+        assert ob_name in more2
+        assert more2[ob_name] == expected_high_count
 
-        # Make sure we do indeed have n more
-        max_excess_py, max_excess_native = max(more_py.values()), max(
-            more_native.values()
-        )
-        assert max_excess_py == n
-        assert max_excess_native == n
+        # It's ok if other objects are created too ...
+
+        # ----- Release
 
         # Delete objects
-        print("  release")
         del objects
         gc.collect()
 
+        # Measure after-release object counts
         counts3 = get_counts()
-        more_py, more_native = compare_counts(counts1, counts3)
-        max_excess_py, max_excess_native = max(more_py.values(), default=0), max(
-            more_native.values(), default=0
-        )
+        more3 = get_excess_counts(counts1, counts3)
+        print("  more after release:", more3)
 
-        print("  more_py", more_py)
-        print("  more_native", more_native)
+        # If no excess objects, all is well!
+        if not more3:
+            return
 
-        assert max_excess_py == 0
-        assert max_excess_native <= 1
-        # assert not more_py
-        # assert not more_native
+        # Otherwise, look deeper
+        expected_low_count = 0, options["allowed_native_low_count"]
+        more3.setdefault(ob_name, (0, 0))
+        assert more3[ob_name] == expected_low_count
 
-    _create_and_release.__name__ = create_objects_func.__name__
-    return _create_and_release
+        # Nothing left other than that?
+        more4 = more3.copy()
+        more4.pop(ob_name)
+        assert not more4
+
+    core_test_func.__name__ = create_objects_func.__name__
+    return core_test_func
 
 
 # %% Meta tests
 
 
 # todo: enable this when done
-def xfail_test_all_objects_covered():
+def xfail_test_meta_all_objects_covered():
     """Test that we have a test_release test function for each known object."""
 
     ref_obnames = set(key.lower() for key in get_counts().keys())
-    func_obnames = set(obname_from_test_func_name(func) for func in RELEASE_TEST_FUNCS)
+    func_obnames = set(ob_name_from_test_func(func) for func in RELEASE_TEST_FUNCS)
 
     missing = ref_obnames - func_obnames
     extra = func_obnames - ref_obnames
@@ -125,31 +172,31 @@ def xfail_test_all_objects_covered():
     assert not extra
 
 
-def test_all_functions_solid():
-    """Test that all funcs starting with test_release are decorated appropriately."""
+def test_meta_all_functions_solid():
+    """Test that all funcs starting with "test_release_" are decorated appropriately."""
     for func in RELEASE_TEST_FUNCS:
-        is_decorated = func.__code__.co_name == "_create_and_release"
+        is_decorated = func.__code__.co_name == "core_test_func"
         assert is_decorated, func.__name__ + " not decorated"
 
 
-def test_buffers_meta1():
+def test_meta_buffers_1():
     """Making sure that the test indeed fails, when holding onto the objects."""
 
     lock = []
 
     @create_and_release
-    def test_buffer(n):
-        device = wgpu.utils.get_default_device()
+    def test_release_buffer(n):
+        yield {}
         for i in range(n):
-            b = device.create_buffer(size=128, usage=wgpu.BufferUsage.COPY_DST)
+            b = DEVICE.create_buffer(size=128, usage=wgpu.BufferUsage.COPY_DST)
             lock.append(b)
             yield b
 
     with pytest.raises(AssertionError):
-        test_buffer()
+        test_release_buffer()
 
 
-def test_buffers_meta2():
+def test_meta_buffers_2():
     """Making sure that the test indeed fails, by disabling the release call."""
 
     ori = wgpu.backends.rs.GPUBuffer._destroy
@@ -165,27 +212,31 @@ def test_buffers_meta2():
 
 # %% The actual tests
 
+# These tests need to do one thing: generate n objects of the correct type.
+
 
 @create_and_release
 def test_release_adapter(n):
+    yield {}
     for i in range(n):
         yield wgpu.request_adapter(canvas=None, power_preference="high-performance")
 
 
 @create_and_release
 def xfail_test_release_device(n):
-    # Device object seem not to be cleaned up at wgpu-native. Wazzup with that?
+    # XFAIL: Device object seem not to be cleaned up at wgpu-native
 
-    adapter = wgpu.utils.get_default_device().adapter
+    yield {}
+    adapter = DEVICE.adapter
     for i in range(n):
         yield adapter.request_device()
 
 
 @create_and_release
 def test_release_bind_group(n):
-    device = wgpu.utils.get_default_device()
+    yield {}
 
-    buffer1 = device.create_buffer(size=128, usage=wgpu.BufferUsage.STORAGE)
+    buffer1 = DEVICE.create_buffer(size=128, usage=wgpu.BufferUsage.STORAGE)
 
     binding_layouts = [
         {
@@ -204,24 +255,23 @@ def test_release_bind_group(n):
         },
     ]
 
-    bind_group_layout = device.create_bind_group_layout(entries=binding_layouts)
+    bind_group_layout = DEVICE.create_bind_group_layout(entries=binding_layouts)
 
     for i in range(n):
-        yield device.create_bind_group(layout=bind_group_layout, entries=bindings)
+        yield DEVICE.create_bind_group(layout=bind_group_layout, entries=bindings)
 
 
 @create_and_release
-def xfail_test_release_bind_group_layout(n):
-    # When we use the same binding layout descriptor, wgpu-native
+def test_release_bind_group_layout(n):
+    # Note: when we use the same binding layout descriptor, wgpu-native
     # re-uses the BindGroupLayout object. On the other hand, it also
     # does not seem to clean them up. Perhaps it just caches them? There
     # are only so many possible combinations, and its just 152 bytes
     # (on Metal) per object.
-    #
-    # * We should somehow allow this case in the test.
-    # * Should we do something similar with *our* BindGroupLayout object?
 
-    device = wgpu.utils.get_default_device()
+    # todo: do we want similar behavior for *our* BindGroupLayout object?
+
+    yield {"expected_native_high_count": 1, "allowed_native_low_count": 1}
 
     binding_layouts = [
         {
@@ -234,79 +284,141 @@ def xfail_test_release_bind_group_layout(n):
     ]
 
     for i in range(n):
-        binding_layouts[0]["binding"] = i
-        yield device.create_bind_group_layout(entries=binding_layouts)
+        # binding_layouts[0]["binding"] = i  # force unique objects
+        yield DEVICE.create_bind_group_layout(entries=binding_layouts)
 
 
 @create_and_release
 def test_release_buffer(n):
-    device = wgpu.utils.get_default_device()
+    yield {}
     for i in range(n):
-        yield device.create_buffer(size=128, usage=wgpu.BufferUsage.COPY_DST)
+        yield DEVICE.create_buffer(size=128, usage=wgpu.BufferUsage.COPY_DST)
 
 
-def test_CanvasContext(n):
-    pass
+@create_and_release
+def test_release_canvas_context_1(n):
+    # Test with offscreen canvases. A context is created, but not a wgpu-native surface.
+
+    yield {"expected_native_high_count": 0}
+
+    for i in range(n):
+        c = wgpu.gui.offscreen.WgpuCanvas()
+        ctx = c.get_context()
+        ctx.configure(device=DEVICE, format="bgra8unorm-srgb")
+        ctx.get_current_texture()  # forces creating a surface
+        yield ctx
 
 
-def test_CommandBuffer(n):
-    pass
+@create_and_release
+def xfail_test_release_canvas_context_2(n):
+    # Test with GLFW canvases.
+
+    # XFAIL: The CanvasContext objects (both the Py side and the native surface) are not properly released,
+    # but they are when I check right afterwards, maybe it needs an event loop iter or glfw poll?
+
+    # todo: revisit this with the new upcoming APIs?
+
+    if not can_use_glfw:
+        pytest.skip("Need glfw for this test")
+
+    from wgpu.gui.glfw import WgpuCanvas, glfw  # noqa
+
+    yield {}
+
+    for i in range(n):
+        c = WgpuCanvas()
+        ctx = c.get_context()
+        ctx.configure(device=DEVICE, format="bgra8unorm-srgb")
+        ctx.get_current_texture()  # forces creating a surface
+        yield ctx
 
 
-def test_CommandEncoder(n):
-    pass
+# @create_and_release
+# def test_release_command_buffer(n):
+#     pass
 
 
-def test_ComputePassEncoder(n):
-    pass
+# @create_and_release
+# def test_release_command_encoder(n):
+#     pass
 
 
-def test_ComputePipeline(n):
-    pass
+# @create_and_release
+# def test_release_compute_pass_encoder(n):
+#     pass
 
 
-def test_PipelineLayout(n):
-    pass
+# @create_and_release
+# def test_release_compute_pipeline(n):
+#     pass
 
 
-def test_QuerySet(n):
-    pass
+# @create_and_release
+# def test_release_pipeline_layout(n):
+#     pass
 
 
-def test_Queue(n):
-    pass
+# @create_and_release
+# def test_release_query_set(n):
+#     pass
 
 
-def test_RenderBundle(n):
-    pass
+# @create_and_release
+# def test_release_queue(n):
+#     pass
 
 
-def test_RenderBundleEncoder(n):
-    pass
+# @create_and_release
+# def test_release_render_bundle(n):
+#     pass
 
 
-def test_RenderPassEncoder(n):
-    pass
+# @create_and_release
+# def test_release_render_bundle_encoder(n):
+#     pass
 
 
-def test_RenderPipeline(n):
-    pass
+# @create_and_release
+# def test_release_render_pass_encoder(n):
+#     pass
 
 
-def test_Sampler(n):
-    pass
+# @create_and_release
+# def test_release_render_pipeline(n):
+#     pass
 
 
-def test_ShaderModule(n):
-    pass
+@create_and_release
+def test_release_sampler(n):
+    yield {}
+    for i in range(n):
+        yield DEVICE.create_sampler()
 
 
-def test_Texture(n):
-    pass
+# @create_and_release
+# def test_release_shader_module(n):
+#     pass
 
 
-def test_TextureView(n):
-    pass
+@create_and_release
+def test_release_texture(n):
+    yield {}
+    for i in range(n):
+        yield DEVICE.create_texture(
+            size=(16, 16, 16),
+            usage=wgpu.TextureUsage.TEXTURE_BINDING,
+            format="rgba8unorm",
+        )
+
+
+@create_and_release
+def test_release_texture_view(n):
+    yield {}
+    texture = DEVICE.create_texture(
+        size=(16, 16, 16), usage=wgpu.TextureUsage.TEXTURE_BINDING, format="rgba8unorm"
+    )
+    for i in range(n):
+        yield texture.create_view()
 
 
 # %% The end
---
+++ tests_mem/testutils.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import subprocess
+
+
+def _determine_can_use_wgpu_lib():
+    # For some reason, since wgpu-native 5c304b5ea1b933574edb52d5de2d49ea04a053db
+    # the process' exit code is not zero, so we test more pragmatically.
+    code = "import wgpu.utils; wgpu.utils.get_default_device(); print('ok')"
+    result = subprocess.run(
+        [
+            sys.executable,
+            "-c",
+            code,
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    print("_determine_can_use_wgpu_lib() status code:", result.returncode)
+    return (
+        result.stdout.strip().endswith("ok")
+        and "traceback" not in result.stderr.lower()
+    )
+
+
+def _determine_can_use_glfw():
+    code = "import glfw;exit(0) if glfw.init() else exit(1)"
+    try:
+        subprocess.check_output([sys.executable, "-c", code])
+    except Exception:
+        return False
+    else:
+        return True
+
+
+can_use_wgpu_lib = _determine_can_use_wgpu_lib()
+can_use_glfw = _determine_can_use_glfw()
+is_ci = bool(os.getenv("CI", None))
