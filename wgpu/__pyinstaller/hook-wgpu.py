10da2d27ae4e5ed9558f32a656d5cc7c7b455a86
Almar Klein <almar@almarklein.org>
2023-11-13 21:55:18 +0100

    Simplify pyinstaller hook

--- wgpu/__pyinstaller/hook-wgpu.py
+++ wgpu/__pyinstaller/hook-wgpu.py
@@ -5,29 +5,20 @@ hiddenimports = []
 datas = []
 binaries = []
 
-# Include our resource data and binaries
+# Include our resource data and binaries.
 datas += collect_data_files("wgpu", subdir="resources")
 binaries += collect_dynamic_libs("wgpu")
 
-# Include the modules that we want to include (PyInstall will trace
-# imports). Note that the __init__.py in resources, utils, backends,
-# and gui subpackages are imported by default.
+# Always include the wgpu-native backend. Since an import is not needed to
+# load this (default) backend, PyInstaller does not see it by itself.
 hiddenimports += ["wgpu.backends.auto", "wgpu.backends.rs"]
-hiddenimports += ["wgpu.gui.auto"]
 
-
-# We have multiple subpackages for which the modules are not imported
-# by default. We make sure that PyInstaller adds them anyway. Strictly
-# speaking this would not be necessary, but it won't hurt, and it covers
-# cases like e.g. downstream libs doing dynamic imports of gui backends.
-for subpackage in ["utils", "backends", "gui"]:
-    datas += collect_data_files(
-        "wgpu", subdir=subpackage, include_py_files=True, excludes=["__pycache__"]
-    )
-
-# For good measure, we include GLFW if we can, so that code that just
-# uses `from wgpu.gui.auto import ..` just works. The glfw library is really
-# small, so there is not much harm.
+# For the GUI backends, there always is an import. The auto backend is
+# problematic because PyInstaller cannot follow it to a specific
+# backend. Also, glfw does not have a hook like this, so it does not
+# include the binary when freezing. We can solve both problems with the
+# code below. Makes the binaray a bit larger, but only marginally (less
+# than 300kb).
 try:
     import glfw  # noqa
 except ImportError:
