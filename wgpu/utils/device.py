6186fff2fcfe053115fb0a6312b2693e3af5b2e0
Jan <jakel101@hhu.de>
2024-06-08 21:39:44 +0200

    add args for default device

--- wgpu/utils/device.py
+++ wgpu/utils/device.py
@@ -1,11 +1,14 @@
 _default_device = None
 
 
-def get_default_device():
+def get_default_device(required_features=[], required_limits={}):
     """Get a wgpu device object. If this succeeds, it's likely that
     the WGPU lib is usable on this system. If not, this call will
     probably exit (Rust panic). When called multiple times,
     returns the same global device object (useful for e.g. unit tests).
+    Parameters:
+        required_features (list of str|`wgpu.FeatureName`): the features (extensions) that you need. Default [].
+        required_limits (dict): the various limits that you need. Default {}.
     """
     global _default_device
 
@@ -13,5 +16,7 @@ def get_default_device():
         import wgpu.backends.auto  # noqa
 
         adapter = wgpu.gpu.request_adapter(power_preference="high-performance")
-        _default_device = adapter.request_device()
+        _default_device = adapter.request_device(
+            required_features=required_features, required_limits=required_limits
+        )
     return _default_device
