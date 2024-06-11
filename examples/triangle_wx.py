b6e4a575f49746f727cffe37e6d8649e43487fb5
panxinmiao <panxinmiao@minivision.cn>
2024-06-11 10:07:47 +0800

    fix wx examples

--- examples/triangle_wx.py
+++ examples/triangle_wx.py
@@ -7,7 +7,7 @@ Import the viz from triangle.py and run it in a wxPython window.
 import wx
 from wgpu.gui.wx import WgpuCanvas
 
-from examples.triangle import main  # The function to call to run the visualization
+from triangle import main  # The function to call to run the visualization
 
 
 app = wx.App()
