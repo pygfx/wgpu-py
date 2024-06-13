991454534b67cf145805873d325886a27c86a380
Almar Klein <almar@almarklein.org>
2024-06-13 10:16:30 +0200

    New wgpu native (#506)

--- wgpu/backends/wgpu_native/__init__.py
+++ wgpu/backends/wgpu_native/__init__.py
@@ -9,8 +9,8 @@ from .. import _register_backend
 
 
 # The wgpu-native version that we target/expect
-__version__ = "0.19.3.1"
-__commit_sha__ = "8f94e257f4abef4e5333fc1b181c2b404d6e34c0"
+__version__ = "0.19.4.1"
+__commit_sha__ = "d89e5a93f5fd0bb3ce28f09571dbbefea37712c9"
 version_info = tuple(map(int, __version__.split(".")))
 _check_expected_version(version_info)  # produces a warning on mismatch
 
