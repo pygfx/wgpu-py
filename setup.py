468e091307bcdade683c55a46d54a57f1180f58a
Frank Yellin <fy@fyellin.com>
2024-08-06 09:57:38 -0700

    Last one.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import platform
 
 from setuptools import find_packages, setup
 from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-from wheel.bdist_wheel.bdist_wheel import get_platform
+from setuptools import get_platform
 
 NAME = "wgpu"
 SUMMARY = "Next generation GPU API for Python"
