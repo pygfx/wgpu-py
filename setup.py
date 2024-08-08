fc7f1f1b9eb2938d93a76e5abfd245437b7e8eae
Frank Yellin <fy@fyellin.com>
2024-08-08 09:02:30 -0500

    Get different get_platform

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import re
 import platform
 
 from setuptools import find_packages, setup
+from setuptools.command.bdist_wheel import get_platform
 from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-from wheel._bdist_wheel import get_platform
 
 NAME = "wgpu"
 SUMMARY = "Next generation GPU API for Python"
