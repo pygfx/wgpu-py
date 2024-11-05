c6210d77ff5d97cc5c9b2c3b3d75603e21b23fbf
Almar Klein <almar@almarklein.org>
2024-11-05 12:55:56 +0100

    Alwats use rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -158,8 +158,7 @@ master_doc = "index"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-if not (os.getenv("READTHEDOCS") or os.getenv("CI")):
-    html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
