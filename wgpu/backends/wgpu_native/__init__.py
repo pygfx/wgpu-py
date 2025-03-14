512ad3674bdb95ab258d593dcba47b3eb9e20790
Almar Klein <almar@almarklein.org>
2025-03-14 09:21:04 +0100

    adapt to new ruff

--- wgpu/backends/wgpu_native/__init__.py
+++ wgpu/backends/wgpu_native/__init__.py
@@ -13,7 +13,7 @@ from .. import _register_backend
 # The wgpu-native version that we target/expect
 __version__ = "22.1.0.5"
 __commit_sha__ = "fad19f5990d8eb9a6e942eb957344957193fe66d"
-version_info = tuple(map(int, __version__.split(".")))
+version_info = tuple(map(int, __version__.split(".")))  # noqa: RUF048
 _check_expected_version(version_info)  # produces a warning on mismatch
 
 # Instantiate and register this backend
