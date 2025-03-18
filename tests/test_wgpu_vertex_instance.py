f3c5c7d04cf21148c1f2591189a68a6f877501ed
Frank Yellin <fy@fyellin.com>
2025-03-18 16:49:50 -0700

    mdi_count can finally work

--- tests/test_wgpu_vertex_instance.py
+++ tests/test_wgpu_vertex_instance.py
@@ -8,9 +8,11 @@ from wgpu import TextureFormat
 from wgpu.backends.wgpu_native.extras import (
     multi_draw_indexed_indirect,
     multi_draw_indirect,
+    multi_draw_indirect_count,
+    multi_draw_indexed_indirect_count,
 )
 
-MAX_INFO = 100
+MAX_INFO = 1000
 
 if not can_use_wgpu_lib:
     pytest.skip("Skipping tests that need the wgpu lib", allow_module_level=True)
@@ -68,7 +70,7 @@ BIND_GROUP_ENTRIES = [
 
 class Runner:
     REQUIRED_FEATURES = ["indirect-first-instance"]
-    OPTIONAL_FEATURES = ["multi-draw-indirect"]  # we'll be adding more
+    OPTIONAL_FEATURES = ["multi-draw-indirect", "multi-draw-indirect-count"]
 
     @classmethod
     def is_usable(cls):
@@ -163,12 +165,24 @@ class Runner:
         # We're going to want to try calling these draw functions from a buffer, and it
         # would be nice to test that these buffers have an offset
         self.draw_data_buffer = self.device.create_buffer_with_data(
-            data=np.uint32([0, 0, *self.draw_args1, *self.draw_args2]),
+            # The zeros at the beginning are to test "offset".
+            # The zeros at the end are because the _count methods require to buffer to
+            # be at least byte_offset + 16 * max_count bytes long
+            data=np.uint32([0, 0, *self.draw_args1, *self.draw_args2, *([0] * 50)]),
             usage="INDIRECT",
         )
         self.draw_data_buffer_indexed = self.device.create_buffer_with_data(
-            data=np.uint32([0, 0, *self.draw_indexed_args1, *self.draw_indexed_args2]),
+            # The zeros at the beginning are to test "offset".
+            # The zeros at the end are because the _count methods require to buffer to
+            # be at least byte_offset + 20 * max_count bytes long
+            data=np.uint32(
+                [0, 0, *self.draw_indexed_args1, *self.draw_indexed_args2, *([0] * 50)]
+            ),
             usage="INDIRECT",
+        )
+
+        self.count_buffer = self.device.create_buffer_with_data(
+            data=(np.int32([10, 2])), usage="INDIRECT"
         )
 
         # And let's not forget our index buffer.
@@ -211,7 +225,8 @@ class Runner:
                 expected_result = self.expected_result_draw_indexed
             else:
                 expected_result = self.expected_result_draw
-        assert info_set == expected_result
+        if info_set != expected_result:
+            pytest.fail(f"Expected {sorted(expected_result)}\nGot {sorted(info_set)}")
 
 
 if not Runner.is_usable():
@@ -335,6 +350,41 @@ def test_draw_indexed_via_encoder(runner):
         runner.run_draw_test(
             lambda encoder: encoder.execute_bundles([render_bundle_encoder]), True
         )
+
+
+@pytest.mark.parametrize("indexed", [False, True])
+@pytest.mark.parametrize("test_max_count", [False, True])
+def test_multi_draw_indirect_count(runner, test_max_count, indexed):
+    if "multi-draw-indirect-count" not in runner.device.features:
+        pytest.skip("Must have 'multi-draw-indirect-count' to run")
+
+    if indexed:
+        function = multi_draw_indexed_indirect_count
+        buffer = runner.draw_data_buffer_indexed
+    else:
+        function = multi_draw_indirect_count
+        buffer = runner.draw_data_buffer
+
+    # The contents of count_buffer is [10, 2] as two 4-byte values.
+    if test_max_count:
+        # We pull the value of 10 from the count buffer, but limit max_count to 2
+        count_buffer_offset, max_count = 0, 2
+    else:
+        # We pull the value of 2 from the count buffer, but set max_count to something
+        # bigger. The buffer is required to be big enough to handle max_count.
+        count_buffer_offset, max_count = 4, 10
+
+    def draw(encoder):
+        function(
+            encoder,
+            buffer,
+            offset=8,
+            count_buffer=runner.count_buffer,
+            count_buffer_offset=count_buffer_offset,
+            max_count=max_count,
+        )
+
+    runner.run_draw_test(draw, indexed)
 
 
 if __name__ == "__main__":
