bee9ba390ef00b672805c8c00e77ff84f2d71039
Ana Dodik <5052480+anadodik@users.noreply.github.com>
2025-06-01 23:11:14 +0000

    Adding missing enums to FeatureName and CanvasAlphaMode.

--- wgpu/enums.py
+++ wgpu/enums.py
@@ -81,6 +81,9 @@ class FeatureName(Enum):
     clip_distances = "clip-distances"
     dual_source_blending = "dual-source-blending"
     subgroups = "subgroups"
+    texture_adapter_specific_format_features = (
+        "texture-adapter-specific-format-features"
+    )
 
 
 class BufferMapState(Enum):
@@ -406,6 +409,7 @@ class QueryType(Enum):
 class CanvasAlphaMode(Enum):
     opaque = "opaque"
     premultiplied = "premultiplied"
+    unpremultiplied = "unpremultiplied"
 
 
 class CanvasToneMappingMode(Enum):
