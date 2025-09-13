4ad4026c9d18003c91e2b6a24726e56a3871386f
Jan <jakel101@hhu.de>
2025-09-14 00:45:12 +0200

    v26 headers

--- wgpu/backends/wgpu_native/__init__.py
+++ wgpu/backends/wgpu_native/__init__.py
@@ -11,8 +11,8 @@ from .. import _register_backend
 
 
 # The wgpu-native version that we target/expect
-__version__ = "25.0.2.2"
-__commit_sha__ = "a2f5109b0da3c87d356a6a876f5b203c6a68924a"
+__version__ = "24.0.4.0"
+__commit_sha__ = "c967f16541ce0948edbb88fc929d8921a8b30f2a"
 version_info = tuple(map(int, __version__.split(".")))  # noqa: RUF048
 _check_expected_version(version_info)  # produces a warning on mismatch
 
