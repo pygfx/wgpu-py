d9305b0b0a2fc44622e5885902b3d958e2394dbc
Almar Klein <almar@almarklein.org>
2026-02-27 13:41:25 +0100

    could it be the join

--- wgpu/backends/wgpu_native/_poller.py
+++ wgpu/backends/wgpu_native/_poller.py
@@ -75,7 +75,7 @@ class PollThread(threading.Thread):
         self._poll_func = lambda _: None
         self._token_ids.clear()
         self._event.set()
-        self.join()
+        # self.join()
 
     def run(self):
         """The thread logic."""
