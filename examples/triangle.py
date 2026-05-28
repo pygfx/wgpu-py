dbdd243966919d93fbf4e6e5c8042171e2a32ac3
Jan <jakel101@hhu.de>
2026-05-28 22:45:07 +0200

    simplify the shadercode a little bit

--- examples/triangle.py
+++ examples/triangle.py
@@ -25,37 +25,34 @@ from rendercanvas.auto import RenderCanvas, loop
 # the shader code is provided as a string literal for protability
 # TODO: maybe simplify in several ways, or make it the red triangle.
 shader_source = """
-struct VertexInput {
-    @builtin(vertex_index) vertex_index : u32,
-};
 struct VertexOutput {
-    @location(0) color : vec4<f32>,
-    @builtin(position) pos: vec4<f32>,
+    @location(0) color : vec4f,
+    @builtin(position) pos: vec4f,
 };
 
 @vertex
-fn vs_main(in: VertexInput) -> VertexOutput {
-    var positions = array<vec2<f32>, 3>(
-        vec2<f32>(0.0, -0.5),
-        vec2<f32>(0.5, 0.5),
-        vec2<f32>(-0.5, 0.75),
+fn vs_main(@builtin(vertex_index) index: u32) -> VertexOutput {
+    var positions = array<vec2f, 3>(
+        vec2(0.0, -0.5),
+        vec2(0.5, 0.5),
+        vec2(-0.5, 0.75),
     );
-    var colors = array<vec3<f32>, 3>(  // srgb colors
-        vec3<f32>(1.0, 1.0, 0.0),
-        vec3<f32>(1.0, 0.0, 1.0),
-        vec3<f32>(0.0, 1.0, 1.0),
+    // vertex attributes are interpolated in the fragment shader.
+    var colors = array<vec3f, 3>(  // srgb colors
+        vec3(1.0, 1.0, 0.0),
+        vec3(1.0, 0.0, 1.0),
+        vec3(0.0, 1.0, 1.0),
     );
-    let index = i32(in.vertex_index);
     var out: VertexOutput;
-    out.pos = vec4<f32>(positions[index], 0.0, 1.0);
-    out.color = vec4<f32>(colors[index], 1.0);
+    out.pos = vec4(positions[index], 0.0, 1.0);
+    out.color = vec4(colors[index], 1.0);
     return out;
 }
 
 @fragment
-fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    let physical_color = pow(in.color.rgb, vec3<f32>(2.2));  // gamma correct
-    return vec4<f32>(physical_color, in.color.a);
+fn fs_main(in: VertexOutput) -> @location(0) vec4f {
+    let physical_color = pow(in.color.rgb, vec3(2.2));  // gamma correct
+    return vec4(physical_color, in.color.a);
 }
 """
 
